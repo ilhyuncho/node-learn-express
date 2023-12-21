186cc83dd57ffdcdaffc934d371e3c8ef4310e21
choilhyun <cihg155@gmail.com>
2023-12-21 18:44:17 +0900

    init2

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  //res.send("hello Exrpess");
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트 대기중");
+});
