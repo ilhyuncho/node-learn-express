8893a844f5b24a0be69c4074d7498399842009e5
choilhyun <cihg155@gmail.com>
2023-12-22 16:39:36 +0900

    [미들웨어] app.use

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ const path = require("path");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+  console.log("모든 요청 실행 미들웨어");
+  next();
+});
+
+app.use(
+  "/",
+  (req, res, next) => {
+    console.log("get요청 미들웨어");
+    next();
+  }
+  //   (req, res) => {
+  //     throw new Error("에러는 에러 처리 미들웨어로 감");
+  //   }
+);
+
 app.get("/", (req, res) => {
-  //res.send("hello Exrpess");
+  console.log("get요청만 실행");
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/test", (req, res) => {
+  console.log("get test 요청만 실행");
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+// 에러 처리
+app.use((err, req, res, next) => {
+  console.log("get요청 미들웨어");
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get("port"), () => {
