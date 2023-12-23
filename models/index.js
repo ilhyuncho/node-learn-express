8dbfb80d6b25792a13131f0d595acf7c7e82e3e1
choilhyun <cihg155@gmail.com>
2023-12-23 16:30:21 +0900

    [DB연결] Model 정의하기

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require("sequelize");
+const User = require("./user");
+const Comment = require("./comment");
 
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
@@ -12,5 +14,14 @@ const sequelize = new Sequelize(
 );
 
 db.sequelize = sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.initiate(sequelize);
+Comment.initiate(sequelize);
+
+User.associate(db); // associate 는 다른 테이블과의 관계를 연결
+Comment.associate(db);
 
 module.exports = db;
